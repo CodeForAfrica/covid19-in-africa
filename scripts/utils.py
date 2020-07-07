aeea1274e9911edffece98770b2327d400093e92
4bic <robyne.kiplangat@gmail.com>
2020-07-07 11:55:33 +0300

    Revert "update Cape Verde > Cabo Verde"
    This reverts commit 51711fcd664d81790c6cb3ef03f98e7ec9151cad.

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,5 +1,5 @@
 
-Africa = ["Algeria", "Angola", "Benin", "Botswana", "Burkina Faso", "Burundi", "Cameroon", "Cabo Verde", "Central African Republic", "Chad",
+Africa = ["Algeria", "Angola", "Benin", "Botswana", "Burkina Faso", "Burundi", "Cameroon", "Cape Verde", "Central African Republic", "Chad",
 "Comoros", "Democratic Republic of the Congo", "Republic of the Congo", "Djibouti", "Egypt", "Equatorial Guinea", "Eritrea", "Ethiopia", "Gabon", 
 "Gambia", "Ghana", "Guinea", "Guinea-Bissau", "Ivory Coast", "Kenya", "Lesotho", "Liberia", "Libya", "Madagascar", "Malawi", "Mali", "Mauritania", 
 "Mauritius", "Morocco", "Mozambique", "Namibia", "Niger", "Nigeria", "Rwanda", "Sao Tome and Principe", "Senegal", "Seychelles", "Sierra Leone", 
