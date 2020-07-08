ace2e889446dc71e147bb1c24b9e454db30a2b81
4bic <robyne.kiplangat@gmail.com>
2020-07-08 20:50:20 +0300

    countries update

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,6 +1,6 @@
 
 Africa = ["Algeria", "Angola", "Benin", "Botswana", "Burkina Faso", "Burundi", "Cameroon", "Cabo Verde", "Central African Republic", "Chad",
-"Comoros", "Democratic Republic of the Congo", "Republic of the Congo", "Djibouti", "Egypt", "Equatorial Guinea", "Eritrea", "Ethiopia", "Gabon",
-"Gambia", "Ghana", "Guinea", "Guinea-Bissau", "Ivory Coast", "Kenya", "Lesotho", "Liberia", "Libya", "Madagascar", "Malawi", "Mali", "Mauritania",
-"Mauritius", "Morocco", "Mozambique", "Namibia", "Niger", "Nigeria", "Rwanda", "Sao Tome and Principe", "Senegal", "Seychelles", "Sierra Leone",
-"Somalia", "South Africa", "South Sudan", "Sudan", "Swaziland", "Tanzania", "Togo", "Tunisia", "Uganda", "Zambia", "Zimbabwe"]
+"Comoros","Congo (Brazzaville)","Congo (Kinshasa)","Cote d'Ivoire", "Democratic Republic of the Congo", "Republic of the Congo", "Djibouti", "Egypt",
+"Equatorial Guinea", "Eritrea", "Ethiopia","Eswatini", "Gabon","Gambia", "Ghana", "Guinea", "Guinea-Bissau", "Ivory Coast", "Kenya", "Lesotho", "Liberia", 
+"Libya", "Madagascar", "Malawi", "Mali", "Mauritania","Mauritius", "Morocco", "Mozambique", "Namibia", "Niger", "Nigeria", "Rwanda", "Sao Tome and Principe", 
+"Senegal", "Seychelles", "Sierra Leone","Somalia", "South Africa", "South Sudan", "Sudan", "Swaziland", "Tanzania", "Togo", "Tunisia", "Uganda", "Western Sahara","Zambia", "Zimbabwe"]
