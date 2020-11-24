3e3931df19551b32c04046f3ab04e9ba88641559
Tim-Abwao <abwaomusungu@gmail.com>
2020-11-24 11:03:33 +0300

    Improve code readability

--- scripts/african_cases_full.py
+++ scripts/african_cases_full.py
@@ -3,47 +3,59 @@ import utils
 
 base_url = """https://raw.githubusercontent.com/CSSEGISandData/COVID-19/\
 master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_"""
+
 url_dict = {
     "Confirmed": f"{base_url}confirmed_global.csv",
     "Deaths": f"{base_url}deaths_global.csv",
     "Recovered": f"{base_url}recovered_global.csv"
 }
-unwanted_cols = ['Province/State', 'Lat', 'Long']
 
 
-def fetch_data(name, url):
+def fetch_data(label, url):
     """Fetch data from the John Hopkins API.
 
     Parameters:
     ----------
-    name: str
-        A label; one of 'Confirmed', 'Recovered' or 'Deaths' as per url.
+    label: str
+        One of 'Confirmed', 'Recovered' or 'Deaths'.
     url : str
-        A link to a data set.
+        A link to the data set.
 
     Returns:
     -------
-    A DataFrame of the data in the supplied url.
+    A Series of the data in the supplied url, indexed by date and country.
     """
+    # Gather global data for the given label
+    unwanted_cols = ['Province/State', 'Lat', 'Long']
     global_cases = pd.read_csv(url).drop(unwanted_cols, axis=1)
+
+    # Select data for African countries
     africa_df = global_cases[global_cases['Country/Region'].isin(utils.Africa)]
-    africa_df = africa_df.set_index('Country/Region').unstack()
-    return africa_df.rename(name)
+    africa_df = africa_df.set_index('Country/Region')
+
+    # Create a pivot table with date & country as a MultiIndex, and the data
+    # as a series.
+    data_series = africa_df.unstack()
+    return data_series.rename(label)
 
 
 def compile_africa_data(url_dict):
-    """Get and save the combined historic and daily Africa coronavirus data,
-    then plot basic visualisations.
+    """Get and save the historic and daily Africa coronavirus data, then plot
+    basic visualisations.
 
     Parameters:
     ----------
     url_dict: dict
-        A dictionary of 'name': 'url' pairs.
+        A dictionary of 'label': 'url' pairs.
     """
-    cases_list = [fetch_data(name, url) for name, url in url_dict.items()]
+    # Create a list of Series for confirmed, recovered and death cases
+    cases_list = [fetch_data(label, url) for label, url in url_dict.items()]
 
+    # Combine the data as a DataFrame. Reset the index so that country and
+    # date are restored as columns.
     combined = pd.concat(cases_list, axis=1).reset_index()
 
+    # Package the data as per the format in the archives, and save it.
     africa_historic = pd.DataFrame({
         'Country/Region': combined['Country/Region'],
         'Date': pd.to_datetime(combined['level_0']).dt.strftime('%m-%d-%Y'),
@@ -53,19 +65,23 @@ def compile_africa_data(url_dict):
     })
     africa_historic.to_csv('./datasets/africa_historic_data.csv', index=False)
 
+    # Extract data for the latest date.
     dates = africa_historic['Date']
     africa_daily = africa_historic[dates == dates.max()]
+
+    # Sort in descending order of confirmed cases, and save.
     africa_daily = africa_daily.sort_values(by='Confirmed', ascending=False)
     filename = f'./datasets/daily/{dates.max()}_c19_african_cases.csv'
     africa_daily.to_csv(filename, index=False)
 
+    # Fetch location data for use in geographical plots
     coordinates = pd.read_csv(url_dict['Confirmed'],
                               usecols=['Country/Region', 'Lat', 'Long'])
     geo_data = africa_daily.merge(coordinates, on='Country/Region')
 
-    utils.plot_africa_totals(africa_historic)
-    utils.plot_daily_confirmed(africa_daily)
-    utils.plot_geoscatter(geo_data)
+    utils.plot_africa_totals(africa_historic)  # lineplots of total cases
+    utils.plot_daily_confirmed(africa_daily)  # barplot of cases by country
+    utils.plot_geoscatter(geo_data)  # bubble map of cases in Africa
 
 
 if __name__ == "__main__":
--- scripts/utils.py
+++ scripts/utils.py
@@ -18,47 +18,59 @@ Africa = [
 
 
 def despine(ax, sides=['left', 'right', 'top', 'bottom']):
-    """Removes the bordering box (spines) from Matplotlib axes."""
+    """Remove the bordering box (spines) from Matplotlib axes."""
 
     [ax.spines[side].set_visible(False) for side in sides]
 
 
 def plot_africa_totals(data, colors=['blue', 'orangered', 'lawngreen']):
-    """Create a line plot of historical coronavirus case totals.
+    """Create lineplots of coronavirus case totals in Africa.
 
     Parameters:
     ----------
     data: pd.DateFrame
         A dataframe of historic coronavirus cases in Africa.
-    colors: str, valid marplotlib color input
-        Colors to apply to the 'confirmed', 'deaths', and 'recovered' lines
-        respectively'."""
-
+    colors: str, valid matplotlib color input
+        Colors to apply to the 'confirmed', 'deaths', and 'recovered'
+        lineplots, respectively'.
+    """
+    # Convert Date column values to Timestamps
     data['Date'] = data['Date'].astype('datetime64')
+
+    # Calculate totals for each day. After this, only the columns 'Confirmed'
+    # 'Deaths' and 'Recovered' remain, with 'Date' becoming the index.
     data = data.groupby('Date').sum()
-    latest = data.index.max().strftime('%d %h, %Y')
 
+    latest_date = data.index.max().strftime('%h %d, %Y')  # used in title
+
+    # Create a matplotlib Figure, with lineplots for 'confirmed', 'recovered',
+    # and 'deaths'
     fig = Figure(figsize=(10, 5), dpi=200)
-    fig.suptitle(f'Total Coronavirus Cases in Africa as at {latest}', size=18)
+    fig.suptitle(f'Total Coronavirus Cases in Africa as at {latest_date}',
+                 size=18)
     ax = fig.subplots()
-    # plot lines
-    for idx, col in enumerate(data.columns):
-        column = data[col]
-        ax.plot(column.index, column, lw=2, color=colors[idx], label=col)
+    for idx, label in enumerate(['Confirmed', 'Deaths', 'Recovered']):
+        ax.plot(data[label].index, data[label], lw=2, color=colors[idx],
+                label=label)
 
-    # annotate line plots
-    for ln in ax.lines:
-        x_coord, y_coord = ln.get_xydata()[-1]
-        ax.annotate(f'{ln.get_ydata().max():,}',
+    # Annotate lineplots with current totals
+    for line in ax.lines:
+        x_coord, y_coord = line.get_xydata()[-1]  # position of latest totals
+        ax.annotate(f'{line.get_ydata().max():,}',  # latest total
                     xy=(x_coord+4, y_coord), style='oblique')
 
-    ax.tick_params(axis='x', rotation=60)
-    ax.yaxis.grid()  # horizontal gridlines
+    ax.tick_params(axis='x', rotation=60)  # rotate x-axis ticks (dates) by 60°
+    ax.yaxis.grid()  # draw horizontal gridlines
+    ax.set_ylabel('Total Cases', size=14)
+    despine(ax)  # remove bordering box
+
+    # Show y-axis ticks as full plain text, instead of in scientific notation
     ax.ticklabel_format(axis='y', style='plain')
-    ax.set_ylabel('Total Cases')
+
+    # Set the x-axis to show dates in 2-week intervals
     ax.xaxis.set_major_locator(mdates.WeekdayLocator(interval=2))
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%h %d'))
-    despine(ax)
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%h %d'))  # e.g. Jan 20
+
     fig.legend(loc='right')
     fig.savefig('images/africa_totals.png', bbox_inches='tight',
                 transparent=True)
@@ -70,36 +82,49 @@ def plot_daily_confirmed(daily_data):
     Parameters:
     ----------
     daily_data: pd.DataFrame
-        A dataframe of daily coronavirus cases, with confirmed cases in
-        descending order.
+        A dataframe of daily coronavirus cases, with columns for 'Date',
+        'Country/Region', and 'Confirmed'.
     """
-    data = daily_data.set_index('Country/Region')['Confirmed'].sort_values()
-    date = daily_data['Date'].astype('datetime64').max().strftime('%h %d, %Y')
+    latest_date = daily_data['Date'].astype('datetime64').max()\
+                                    .strftime('%h %d, %Y')
 
+    # Get and sort confirmed cases in ascending order
+    data = daily_data.set_index('Country/Region')['Confirmed'].sort_values()
+
+    # Create a horizontal barplot with country-confirmed-cases as bars
     fig = Figure(figsize=(6, 12))
     ax = fig.subplots()
-
+    ax.set_title(f'Confirmed Coronavirus Cases by Country as at {latest_date}',
+                 size=18)
     data.plot.barh(x='Country/Region', ax=ax, width=0.9, color='teal')
-    # annotate bars
+
+    # Annotate bars with country-confirmed-case tally
     for p in ax.patches:
         ax.annotate(f'{p.get_width():,}', style='oblique',
                     xy=(p.get_x()+p.get_width(), p.get_y()+.2))
-    despine(ax)
-    ax.tick_params(size=15)
+    despine(ax)  # remove bordering box
+    ax.tick_params(size=15)  # set fontsize for ticks (country names)
     ax.get_xaxis().set_visible(False)  # hide x-axis
-    ax.set_title(f'Confirmed Coronavirus Cases by Country as at {date}',
-                 size=18)
+
     fig.savefig('images/africa_daily.png', transparent=True,
                 bbox_inches='tight')
 
 
 def plot_geoscatter(geo_data):
+    """Create a bubble map of coronavirus cases in Africa.
+
+    Parameters:
+    ----------
+    geodata: pd.DataFrame
+        A DataFrame with columns 'Lat' and 'Long', of latitude and longitude
+        values respectively, with corresponding 'Confirmed` case data.
+    """
     fig = px.scatter_geo(
         geo_data, lat='Lat', lon='Long', scope='africa', size='Confirmed',
         color='Confirmed', height=600, width=600,
         color_continuous_scale=['cyan', 'yellow', 'orangered'],
         title='Geographic Scatter-plot of Confirmed Cases'
     )
-
+    # Save geographical scatter-plot as png image file
     with open('images/geo_scatter.png', 'wb') as pic:
         pic.write(fig.to_image(format='png', scale=2))
